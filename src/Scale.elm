88170e9e0e02c31714b16408ac6d647d51e27c6c
Duncan Malashock <duncanmalashock@gmail.com>
2017-04-29 22:55:53 -0400

    Scale.scaleDegree

--- src/Scale.elm
+++ src/Scale.elm
@@ -1,4 +1,8 @@
-module Scale exposing (Scale(..))
+module Scale
+    exposing
+        ( Scale(..)
+        , scaleDegree
+        )
 
 import Note exposing (Note(..))
 
@@ -28,3 +32,45 @@ type alias MinorScaleDegrees =
 type Scale
     = Major MajorScaleDegrees
     | Minor MinorScaleDegrees
+
+
+scaleDegree : Scale -> Int -> Note
+scaleDegree scale degree =
+    let
+        getIndexAt i list =
+            List.head (List.drop i list)
+    in
+        case scale of
+            Major ds ->
+                let
+                    degreeList =
+                        [ ds.firstDegree
+                        , ds.secondDegree
+                        , ds.thirdDegree
+                        , ds.fourthDegree
+                        , ds.fifthDegree
+                        , ds.sixthDegree
+                        , ds.seventhDegree
+                        ]
+                in
+                    Maybe.withDefault ds.firstDegree <|
+                        getIndexAt
+                            ((degree - 1) % (List.length degreeList))
+                            degreeList
+
+            Minor ds ->
+                let
+                    degreeList =
+                        [ ds.firstDegree
+                        , ds.secondDegree
+                        , ds.thirdDegree
+                        , ds.fourthDegree
+                        , ds.fifthDegree
+                        , ds.sixthDegree
+                        , ds.seventhDegree
+                        ]
+                in
+                    Maybe.withDefault ds.firstDegree <|
+                        getIndexAt
+                            ((degree - 1) % (List.length degreeList))
+                            degreeList
